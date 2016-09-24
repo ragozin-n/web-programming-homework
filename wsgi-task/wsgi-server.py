c0b3bd1dd43d153c668393001a5cc9d2d9f7adff
Ragozin Nikita <lastjanuary@yandex.ru>
2016-09-24 13:15:58 +0500

    middleware complete

--- wsgi-task/wsgi-server.py
+++ wsgi-task/wsgi-server.py
@@ -26,8 +26,28 @@ def app(environ, start_response):
 
     start_response(status, response_headers)
     return result
-        
 
+class Middleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        top_str = "<div class='top'>Middleware TOP</div>\n"
+        bottom_str = "<div class='bottom'>Middleware BOTTOM</div>\n"
+        body_open = "<body>"
+        body_close = "</body>"
+
+        for line in self.app(environ, start_response):
+            if line.find(body_open) != -1:
+                yield line
+                yield top_str
+            elif line.find(body_close) != -1:
+                yield bottom_str
+                yield line
+            else:
+                yield line
+
+app = Middleware(app)
 if __name__ == '__main__':
     _server = make_server('localhost', 8000, app)
     print "Serving localhost on port 8000..."
